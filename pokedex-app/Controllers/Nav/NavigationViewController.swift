e2fa5a89271a0a3ed941679eeda7858b38caa63f
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-05-12 20:56:20 -0300

    feat: create main view controllers

---
+++ pokedex-app/Controllers/Nav/NavigationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationViewController.swift
+//  pokedex-app
+//
+//  Created by Leonardo Cardoso on 12/05/23.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
